1f8d2d4142598ca9bb5c1c79a3fb769c6246245c
Ana Luiza Silva Dutra <aninhadutra.nobreak@gmail.com>
2026-09-08 14:03:48 -0300

    alterando de 10 a 100

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let listaDeNumerosSorteados = [];
-let numeroLimite = 10;
+let numeroLimite = 100;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
@@ -11,7 +11,7 @@ function exibirTextoNaTela(tag, texto) {
 
 function exibirMensagemInicial() {
     exibirTextoNaTela('h1', 'Jogo do Número Secreto');
-    exibirTextoNaTela('p', 'Escolha um número de 1 a 10');
+    exibirTextoNaTela('p', 'Escolha um número de 1 a 100');
 }
 
 exibirMensagemInicial();
@@ -36,7 +36,7 @@ function verificarChute() {
 }
 
 function gerarNumeroAleatorio() {
-    let numeroEscolhido = parseInt(Math.random() * 10) + 1;
+    let numeroEscolhido = parseInt(Math.random() * 100) + 1;
     let quantidadeDeElementosNaLista = listaDeNumerosSorteados.length
 
 if (quantidadeDeElementosNaLista == numeroLimite) {
